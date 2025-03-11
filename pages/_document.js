9c2cf6b76622a0e69e2c0cc27d22990c0aa66f7f
Aman Kumar <amanabhay2@gmail.com>
2025-03-11 13:12:12 +0530

    Prevent overscroll behavior on HTML document

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" style={{ overscrollBehaviorY: "none" }}>
       <Head>
         <link
           href="https://fonts.googleapis.com/css2?family=Onest:wght@100..900&family=Sora:wght@100..800&display=swap"
